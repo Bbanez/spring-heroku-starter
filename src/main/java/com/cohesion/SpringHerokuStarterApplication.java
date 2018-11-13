93b8f584bcf6cd116525dc3cbfd8742e911e35e2
Bbanez <banezprod@gmail.com>
2018-11-13 21:51:16 +0100

    Initial commit

---
+++ src/main/java/com/cohesion/SpringHerokuStarterApplication.java
@@ -0,0 +1,12 @@
+package com.cohesion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHerokuStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHerokuStarterApplication.class, args);
+	}
+}
